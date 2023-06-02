335aa7df30bba414f575470ab3783c15904e0500
Gabriela Zarratea <71782924+GabiZarratea@users.noreply.github.com>
2023-06-02 16:13:17 -0300

    Add files via upload

---
+++ assets/js/contact.js
@@ -0,0 +1,6 @@
+function enviarForm(){
+    let name = document.getElementById("name").value
+    let mail = document.getElementById("mail").value
+    let msg = document.getElementById("msg").value
+    alert("We are verifying the correct delivery of the information.")
+}
---
+++ assets/js/upcoming.js
@@ -0,0 +1,28 @@
+let div1 = document.getElementById("card1")
+
+function crearPlantilla(objeto){
+    return  `<div class="card" style="width: 20rem; text-align: center;" >
+            <img src="${objeto.image}" class="img_cards rounded-top-2">
+            <div class="card-body">
+            <h5 class="card-title">${objeto.name}</h5>
+            <p class="card-text">${objeto.description}</p>
+            </div>
+            <div class="card-body d-flex justify-content-around">
+            <p>Price: $ ${objeto.price}</p>
+            <a href="../pages/details.html" class="btn btn-danger btn-details">Details</a>
+            </div>
+            </div>`
+}
+function imprimirDatos(array,lugar){
+    let template = " "
+
+    for(let event of array){
+        if(data.currentDate < event.date){
+        template += crearPlantilla(event)    
+        }
+    }
+    console.log(template)
+    lugar.innerHTML += template
+}
+
+imprimirDatos(data.events, div1)
